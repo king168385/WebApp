f27aeafd4655cdbc2f46bc9a56f33bcab2d4f49d
Michael <Michael@192.168.100.155>
2019-04-19 12:14:01 -0400

    Spring Rest

--- Spring/src/main/java/com/lean/Spring/hb/dao/CustomerRepository.java
+++ Spring/src/main/java/com/lean/Spring/hb/dao/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.lean.Spring.hb.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.lean.Spring.hb.entity.Customer;
 
+@RepositoryRestResource(path="customers")
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
 }
