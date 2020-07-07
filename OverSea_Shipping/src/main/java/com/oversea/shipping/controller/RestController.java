95962bd26533bf3d1df3478c56149dc01ee8a4eb
John Long <jlong@leanindustries.com>
2020-07-07 15:38:09 -0400

    JL: ajax call for shipping status success with csrf token

--- OverSea_Shipping/src/main/java/com/oversea/shipping/controller/RestController.java
+++ OverSea_Shipping/src/main/java/com/oversea/shipping/controller/RestController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.oversea.shipping.model.Shipment;
@@ -14,18 +17,27 @@ import com.oversea.shipping.model.ShipmentPackageStatus;
 import com.oversea.shipping.service.ShipmentService;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/rest")
 public class RestController {
     
     @Autowired
     private ShipmentService shipmentService;
 
 //    @RequestMapping(value = "/shippingStatus", params = {"trackingNumber"}, method = RequestMethod.POST, produces = "application/json")
-    @PostMapping(value = "/shippingstatus/{trackingnumber}", produces = "application/json")
+    @PostMapping(value = "/shippingstatus")
     @ResponseBody
-    public List<ShipmentPackageStatus> getShippingStatusList(@PathVariable(value = "trackingnumber") String trackingNumber) {
+    public List<ShipmentPackageStatus> getShippingStatusList(@RequestParam("trackingnumber") String trackingnumber) {
         
-        Shipment shipment = shipmentService.findByTrackingNumber(trackingNumber);
+        Shipment shipment = shipmentService.findByTrackingNumber(trackingnumber);
+        List<ShipmentPackageStatus> packageStatusList= shipment.getPackageStatusList();
+        
+        return packageStatusList;
+    }
+    
+    @GetMapping("/shipping/{trackingnumber}")
+    @ResponseBody
+    public List<ShipmentPackageStatus> getShippingList(@PathVariable(value = "trackingnumber") String trackingnumber) {
+        Shipment shipment = shipmentService.findByTrackingNumber(trackingnumber);
         List<ShipmentPackageStatus> packageStatusList= shipment.getPackageStatusList();
         
         return packageStatusList;
