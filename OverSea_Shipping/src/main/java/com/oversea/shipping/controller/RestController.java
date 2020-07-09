58fbc0758892f2f07b72fd22764c47e8017d1e66
John Long <jlong@leanindustries.com>
2020-07-08 21:49:04 -0400

    JL: bootstrap-table showing json data with get method

--- OverSea_Shipping/src/main/java/com/oversea/shipping/controller/RestController.java
+++ OverSea_Shipping/src/main/java/com/oversea/shipping/controller/RestController.java
@@ -4,11 +4,13 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -42,7 +44,8 @@ public class RestController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping(value = "/shippingstatus")
+    //@PostMapping(value = "/shippingstatus")
+    @RequestMapping(value = "/shippingstatus", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public List<ShipmentPackageStatus> getShippingStatusList(@RequestParam("trackingnumber") String trackingNumber) {
         
@@ -71,11 +74,35 @@ public class RestController {
         return packageStatusList;
     }
     
-    @GetMapping("/shipping/{trackingnumber}")
+    //@GetMapping("/shipping/{trackingnumber}")
+    @RequestMapping(value = "/shipping/{trackingnumber}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<ShipmentPackageStatus> getShippingList(@PathVariable(value = "trackingnumber") String trackingnumber) {
-        Shipment shipment = shipmentService.findByTrackingNumber(trackingnumber);
+    public List<ShipmentPackageStatus> getShippingList(@PathVariable(value = "trackingnumber") String trackingNumber) {
+        
+        Shipment shipment = shipmentService.findByTrackingNumber(trackingNumber);
         List<ShipmentPackageStatus> packageStatusList= shipment.getPackageStatusList();
+        
+        /*User user = userService.getCurrentUser();
+
+        Collection<Role> roles = user.getRoles();
+        boolean isAdmin = roles.contains(roleRepository.findByName(Role.ADMIN));
+
+        Shipment shipment = null;
+        if (isAdmin) {
+            shipment = shipmentService.findByTrackingNumber(trackingNumber);
+        } else {
+            Customer customer = user.getCustomer();
+            shipment = shipmentRepository.findByCustomerAndTrackingNumber(customer, trackingNumber);
+        }
+
+        List<ShipmentPackageStatus> packageStatusList = null;
+        if (shipment != null) {
+            // TODO
+            // login with: member@test.com
+            // search tracking number: 773044700377098
+            // only return 1 records, it's expecting 2 records (2 records in hipment_package_status table)
+            packageStatusList = shipment.getPackageStatusList();
+        }*/
         
         return packageStatusList;
     }
