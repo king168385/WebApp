bc01146a78f925743d2f1d817bd31dbef94e32eb
leiji <leiji@192.168.100.156>
2020-07-12 01:35:57 -0400

    MJ: change dashboard redirect

--- OverSea_Shipping/src/main/java/com/oversea/shipping/controller/DashboardController.java
+++ OverSea_Shipping/src/main/java/com/oversea/shipping/controller/DashboardController.java
@@ -1,27 +1,47 @@
 package com.oversea.shipping.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.oversea.shipping.auth.service.UserService;
+import com.oversea.shipping.model.Role;
+import com.oversea.shipping.model.Shipment;
+import com.oversea.shipping.model.User;
+
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 	
-	// add mapping for "/list"
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping()
 	public String loadupDashboard(Model theModel) {
+		User user = userService.getCurrentUser();
+
+		if(user.hasRole(Role.ADMIN)) {
+			return "dashboard/dashboard";
+		}else {
+			return "dashboard/dashboard-welcome";
+		}
 		
-		return "dashboard/dashboard-welcome";
 	}
 	
 	@PostMapping()
 	public String login(Model theModel) {
-		
-		return "dashboard/dashboard-welcome";
+		User user = userService.getCurrentUser();
+
+		if(user.hasRole(Role.ADMIN)) {
+			return "dashboard/dashboard";
+		}else {
+			return "dashboard/dashboard-welcome";
+		}
 	}
 }
 
